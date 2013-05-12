c292da667877bd47f59eae5be8619237c1f93246
castrinho8 <pablo.castro1@udc.es>
2013-05-13 01:26:07 +0200

    Añadidos cambios en el diseño para adaptarlo a la implementacion y algunos errores que detecté al hacer el paso

--- doc/creacion_bd_consorcio.sql
+++ doc/creacion_bd_consorcio.sql
@@ -2,6 +2,7 @@
 -- tacod Codigo tarjeta
 CREATE TABLE Tarjeta(
 	codTarjeta VARCHAR(11),
+	tagastoOffline INTEGER,
 	CONSTRAINT ta_pk PRIMARY KEY (codTarjeta)
 );
 
@@ -10,11 +11,22 @@ CREATE TABLE Tarjeta(
 -- cusaldo Saldo cuenta
 CREATE TABLE Cuenta(
 	codCuenta INTEGER,
-	codTarjeta VARCHAR(11),
-	CONSTRAINT cu_pk PRIMARY KEY (codCuenta,codTarjeta)
+	cusaldo INTEGER DEFAULT 0,
+	CONSTRAINT cu_pk PRIMARY KEY (codCuenta)
 );
 
-/*
+
+CREATE TABLE CuentaTarjeta(
+	codTarjeta VARCHAR(11),
+	cunum INTEGER,
+	codCuenta INTEGER,
+	CONSTRAINT ct_ccod_fk FOREIGN KEY (codCuenta) REFERENCES Cuenta(codCuenta) ON DELETE CASCADE,
+	CONSTRAINT ct_tcod_fk FOREIGN KEY (codTarjeta) REFERENCES Tarjeta(codTarjeta) ON DELETE CASCADE,
+	CONSTRAINT ct_cnum_check CHECK (cnum IN (1,3)),
+	CONSTRAINT ct_pk PRIMARY KEY (codTarjeta,cunum)
+);
+
+
 -- tacod Codigo Tarjeta
 -- ctnum Numero entre 1 y 3 que indica las 3 cuentas de cada tarjeta
 -- cucod Codigo de Cuenta
@@ -27,7 +39,6 @@ CREATE TABLE CuentaTarjeta(
 	CONSTRAINT ct_ctnum_check CHECK (ctnum IN (1,3)),
 	CONSTRAINT ct_pk PRIMARY KEY (codTarjeta,ctnum)
 );
-*/
 
 -- tmcod Cogido de tipo de movimiento
 -- tmnombre Nombre del tipo 
@@ -69,17 +80,16 @@ CREATE TABLE Banco(
 -- moimporte Importe del movimiento
 -- mooffline Booleano que indica si es offline
 CREATE TABLE Movimiento(
-	codMovimiento INTEGER,
+	codMovimiento INTEGER AUTO_INCREMENT,
 	codCuentaOrig INTEGER,
-	codTarjetaOrig VARCHAR(11),
 	codCuentaDest INTEGER,
-	codTarjetaDest VARCHAR(11),
 	codTMovimiento INTEGER,
+	mofecha DATE,
 	moimporte INTEGER,
 	mooffline BOOLEAN,
 	codBanco INTEGER,
-	CONSTRAINT mo_codCuentaOrig_fk FOREIGN KEY (codCuentaOrig,codTarjetaOrig) REFERENCES Cuenta(codCuenta,codTarjeta) ON DELETE NO ACTION,
-	CONSTRAINT mo_codCuentaDest_fk FOREIGN KEY (codCuentaDest,codTarjetaDest) REFERENCES Cuenta(codCuenta,codTarjeta) ON DELETE NO ACTION,
+	CONSTRAINT mo_codCuentaOrig_fk FOREIGN KEY (codCuentaOrig) REFERENCES Cuenta(codCuenta) ON DELETE NO ACTION,
+	CONSTRAINT mo_codCuentaDest_fk FOREIGN KEY (codCuentaDest) REFERENCES Cuenta(codCuenta) ON DELETE NO ACTION,
 
 	CONSTRAINT mo_codTMovimiento_fk FOREIGN KEY (codTMovimiento) REFERENCES TipoMovimiento(codTMovimiento) ON DELETE SET NULL,
 
@@ -100,7 +110,6 @@ CREATE TABLE Mensaje(
 	codMensaje INTEGER,
 	meoffline BOOLEAN,
 	mees_envio BOOLEAN,
-
 	codBanco INTEGER,
 	CONSTRAINT me_codBanco_fk FOREIGN KEY (codBanco) REFERENCES Banco(codBanco) ON DELETE NO ACTION,
 
@@ -116,11 +125,10 @@ CREATE TABLE UltimoEnvio(
 	uepuerto INTEGER,
 	ueip VARCHAR(20),
 	codBanco INTEGER,
-	codTarjeta VARCHAR(11),
 	codCuenta INTEGER,
 
 	CONSTRAINT ue_codBanco_fk FOREIGN KEY (codBanco) REFERENCES Banco(codBanco) ON DELETE NO ACTION,
-	CONSTRAINT ue_Cuenta_fk FOREIGN KEY (codCuenta,codTarjeta) REFERENCES Cuenta(codCuenta,codTarjeta) ON DELETE NO ACTION,
+	CONSTRAINT ue_Cuenta_fk FOREIGN KEY (codCuenta) REFERENCES Cuenta(codCuenta) ON DELETE NO ACTION,
 
 	CONSTRAINT ue_pk PRIMARY KEY (codUltimoEnvio)
 );
@@ -137,6 +145,46 @@ CREATE TABLE Canal(
 
 
 
+-- Estados del banco
+
+INSERT INTO EstadoBanco VALUES(1,'Abierta');
+INSERT INTO EstadoBanco VALUES(2,'Cerrada');
+INSERT INTO EstadoBanco VALUES(3,'Trafico detenido');
+INSERT INTO EstadoBanco VALUES(4,'En recuperacion');
+
+-- Bancos
+INSERT INTO Banco(codBanco,codEBanco,bapuerto,baip,bamaxCanales) VALUES(1,1,80,'127.0.0.1',5);
+INSERT INTO Banco(codBanco,codEBanco,bapuerto,baip,bamaxCanales) VALUES(2,1,80,'127.0.0.1',5);
+INSERT INTO Banco(codBanco,codEBanco,bapuerto,baip,bamaxCanales) VALUES(3,2,80,'127.0.0.1',2);
+INSERT INTO Banco(codBanco,codEBanco,bapuerto,baip,bamaxCanales) VALUES(4,1,80,'127.0.0.1',2);
+
+-- Tarjetas
+
+INSERT INTO Tarjeta VALUES ('pastor42 01',0);
+INSERT INTO Tarjeta VALUES ('pastor42 02',0);
+INSERT INTO Tarjeta VALUES ('pastor42 03',0);
+INSERT INTO Tarjeta VALUES ('pastor42 04',0);
+INSERT INTO Tarjeta VALUES ('pastor42 05',0);
+
+
+-- Cuentas
+
+INSERT INTO Cuenta(codCuenta) VALUES (0000);
+INSERT INTO Cuenta(codCuenta) VALUES (0001);
+INSERT INTO Cuenta(codCuenta) VALUES (0002);
+INSERT INTO Cuenta(codCuenta) VALUES (0003);
+INSERT INTO Cuenta(codCuenta) VALUES (0005);
+INSERT INTO Cuenta(codCuenta) VALUES (0010);
+
+
+-- CuentaTarjeta
+INSERT INTO CuentaTarjeta(codTarjeta,cunum,codCuenta) VALUES ('pastor42 01',1,0000);
+INSERT INTO CuentaTarjeta(codTarjeta,cunum,codCuenta) VALUES ('pastor42 01',2,0001);
+INSERT INTO CuentaTarjeta(codTarjeta,cunum,codCuenta) VALUES ('pastor42 02',1,0002);	
+INSERT INTO CuentaTarjeta(codTarjeta,cunum,codCuenta) VALUES ('pastor42 02',2,0003);
+INSERT INTO CuentaTarjeta(codTarjeta,cunum,codCuenta) VALUES ('pastor42 02',3,0005);
+INSERT INTO CuentaTarjeta(codTarjeta,cunum,codCuenta) VALUES ('pastor42 03',1,0010);
+
 -- Tipos de movimento.
 
 INSERT INTO TipoMovimiento VALUES(10,'Reintegro');
@@ -147,13 +195,38 @@ INSERT INTO TipoMovimiento VALUES(50,'Abono');
 INSERT INTO TipoMovimiento VALUES(51,'Cobro de cheque');
 INSERT INTO TipoMovimiento VALUES(99,'Otros');
 
+SELECT codMovimiento,moimporte,mofecha,codTMovimiento FROM Movimiento WHERE ((codCuentaOrig = 2) || (codCuentaDest = 2));
 
--- Estados del banco
+-- Movimientos
 
-INSERT INTO EstadoBanco VALUES(1,'Abierta');
-INSERT INTO EstadoBanco VALUES(2,'Cerrada');
-INSERT INTO EstadoBanco VALUES(3,'Trafico detenido');
-INSERT INTO EstadoBanco VALUES(4,'En recuperacion');
+INSERT INTO Movimiento(codTMovimiento,codCuentaOrig,codCuentaDest,moimporte,mooffline,mofecha,codBanco) 
+VALUES (10,0000,0001,50,0,STR_TO_DATE('05/01/2010', '%m/%d/%Y'),1);
+
+INSERT INTO Movimiento(codTMovimiento,codCuentaOrig,codCuentaDest,moimporte,mooffline,mofecha,codBanco) 
+VALUES (11,0000,0001,10,0,STR_TO_DATE('05/01/2010', '%m/%d/%Y'),1);
+
+INSERT INTO Movimiento(codTMovimiento,codCuentaOrig,codCuentaDest,moimporte,mooffline,mofecha,codBanco) 
+VALUES (50,0000,0001,20,1,STR_TO_DATE('05/04/2010', '%m/%d/%Y'),1);
+
+INSERT INTO Movimiento(codTMovimiento,codCuentaOrig,codCuentaDest,moimporte,mooffline,mofecha,codBanco) 
+VALUES (51,0000,0001,10,0,STR_TO_DATE('04/01/2011', '%m/%d/%Y'),1);
+
+INSERT INTO Movimiento(codTMovimiento,codCuentaOrig,codCuentaDest,moimporte,mooffline,mofecha,codBanco) 
+VALUES (51,0003,0005,10,0,STR_TO_DATE('05/02/2010', '%m/%d/%Y'),1);
+
+
+INSERT INTO Movimiento(codTMovimiento,codCuentaOrig,codCuentaDest,moimporte,mooffline,mofecha,codBanco) 
+VALUES(null,null,null,null,null,null,null);
+
+
+
+
+
+
+
+
+
+
 
 
 
