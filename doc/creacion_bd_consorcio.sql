072c8352159fc5f05e9fdaa35809721805b4ea7f
castrinho8 <pablo.castro1@udc.es>
2013-05-12 17:29:48 +0200

    Acabadas las sentencias de creacion de las tablas y añadidos inserts para las tablas de tipo de movimientos y estados del banco

--- doc/creacion_bd_consorcio.sql
+++ doc/creacion_bd_consorcio.sql
@@ -55,7 +55,7 @@ CREATE TABLE Banco(
 	codBanco INTEGER,
 	codEBanco INTEGER,
 	bapuerto INTEGER,
-	baip		VARCHAR(20),
+	baip VARCHAR(20),
 	bamaxCanales INTEGER,
 	CONSTRAINT ba_codEBanco_fk FOREIGN KEY (codEBanco) REFERENCES EstadoBanco(codEBanco) ON DELETE SET NULL,
 
@@ -71,17 +71,19 @@ CREATE TABLE Banco(
 CREATE TABLE Movimiento(
 	codMovimiento INTEGER,
 	codCuentaOrig INTEGER,
+	codTarjetaOrig VARCHAR(11),
 	codCuentaDest INTEGER,
+	codTarjetaDest VARCHAR(11),
 	codTMovimiento INTEGER,
 	moimporte INTEGER,
 	mooffline BOOLEAN,
-	codBanco CHAR(50),
-	CONSTRAINT mo_codCuentaOrig_fk FOREIGN KEY (codCuentaOrig) REFERENCES Cuenta(codCuenta) ON DELETE CASCADE,
-	CONSTRAINT mo_codCuentaDest_fk FOREIGN KEY (codCuentaDest) REFERENCES Cuenta(codCuenta) ON DELETE CASCADE,
+	codBanco INTEGER,
+	CONSTRAINT mo_codCuentaOrig_fk FOREIGN KEY (codCuentaOrig,codTarjetaOrig) REFERENCES Cuenta(codCuenta,codTarjeta) ON DELETE NO ACTION,
+	CONSTRAINT mo_codCuentaDest_fk FOREIGN KEY (codCuentaDest,codTarjetaDest) REFERENCES Cuenta(codCuenta,codTarjeta) ON DELETE NO ACTION,
 
 	CONSTRAINT mo_codTMovimiento_fk FOREIGN KEY (codTMovimiento) REFERENCES TipoMovimiento(codTMovimiento) ON DELETE SET NULL,
 
-	CONSTRAINT mo_codBanco_fk FOREIGN KEY (codBanco) REFERENCES Banco(codBanco) ON DELETE SET NULL,
+	CONSTRAINT mo_codBanco_fk FOREIGN KEY (codBanco) REFERENCES Banco(codBanco) ON DELETE NO ACTION,
 
 	CONSTRAINT mo_pk PRIMARY KEY (codMovimiento)
 );
@@ -92,45 +94,44 @@ CREATE TABLE Movimiento(
 -- mees_envio Booleano que indica si es envio o no
 -- 
 -- codBanco El codigo del banco correspondiente al mensaje
+
+--	++++++++++++++++ mensaje
 CREATE TABLE Mensaje(
 	codMensaje INTEGER,
 	meoffline BOOLEAN,
 	mees_envio BOOLEAN,
---	++++++++++++++++ mensaje
-	codBanco CHAR(50),
-	CONSTRAINT me_codBanco_fk FOREIGN KEY (codBanco) REFERENCES Banco(codBanco) ON DELETE SET NULL,
 
-	CONSTRAINT me_pk PRIMARY KEY (codMensaje),
+	codBanco INTEGER,
+	CONSTRAINT me_codBanco_fk FOREIGN KEY (codBanco) REFERENCES Banco(codBanco) ON DELETE NO ACTION,
+
+	CONSTRAINT me_pk PRIMARY KEY (codMensaje)
 );
 
 
+--	++++++++++++++++ mensaje
 CREATE TABLE UltimoEnvio(
 	codUltimoEnvio INTEGER,
---	++++++++++++++++ mensaje
 	uecontestado BOOLEAN DEFAULT 0,
 	uecodCajero INTEGER,
 	uepuerto INTEGER,
 	ueip VARCHAR(20),
-	codBanco CHAR(50),
-	codCanal INTEGER,
+	codBanco INTEGER,
 	codTarjeta VARCHAR(11),
 	codCuenta INTEGER,
 
-	CONSTRAINT ue_codBanco_fk FOREIGN KEY (codBanco) REFERENCES Banco(codBanco) ON DELETE SET NULL,
-	CONSTRAINT ue_codCanal_fk FOREIGN KEY (codCanal) REFERENCES Canal(codCanal) ON DELETE SET NULL,
+	CONSTRAINT ue_codBanco_fk FOREIGN KEY (codBanco) REFERENCES Banco(codBanco) ON DELETE NO ACTION,
+	CONSTRAINT ue_Cuenta_fk FOREIGN KEY (codCuenta,codTarjeta) REFERENCES Cuenta(codCuenta,codTarjeta) ON DELETE NO ACTION,
 
-	CONSTRAINT ue_Cuenta_fk FOREIGN KEY (codCuenta,codTarjeta) REFERENCES Cuenta(codCuenta,codTarjeta) ON DELETE SET NULL,
-
-	CONSTRAINT ue_pk PRIMARY KEY (codUltimoEnvio),
+	CONSTRAINT ue_pk PRIMARY KEY (codUltimoEnvio)
 );
 
 CREATE TABLE Canal(
-	codBanco CHAR(50),
+	codBanco INTEGER,
 	codCanal INTEGER,
 	cabloqueado BOOLEAN DEFAULT 0,
 	codUltimoEnvio INTEGER DEFAULT NULL,
-	CONSTRAINT me_codUltimoEnvio_fk FOREIGN KEY (codUltimoEnvio) REFERENCES UltimoEnvio(codUltimoEnvio) ON DELETE SET NULL,
-	CONSTRAINT ca_pk PRIMARY KEY (codBanco,codCanal),
+	CONSTRAINT me_codUltimoEnvio_fk FOREIGN KEY (codUltimoEnvio) REFERENCES UltimoEnvio(codUltimoEnvio) ON DELETE CASCADE,
+	CONSTRAINT ca_pk PRIMARY KEY (codBanco,codCanal)
 );
 
 
@@ -138,13 +139,37 @@ CREATE TABLE Canal(
 
 -- Tipos de movimento.
 
-INSERT INTO TipoMovemento VALUES(10,'Reintegro');
-INSERT INTO TipoMovemento VALUES(11,'Transpaso emitido');
-INSERT INTO TipoMovemento VALUES(12,'Transpaso recibido');
-INSERT INTO TipoMovemento VALUES(13,'Pago recibo');
-INSERT INTO TipoMovemento VALUES(50,'Abono');
-INSERT INTO TipoMovemento VALUES(51,'Cobro de cheque');
-INSERT INTO TipoMovemento VALUES(99,'Otros');
+INSERT INTO TipoMovimiento VALUES(10,'Reintegro');
+INSERT INTO TipoMovimiento VALUES(11,'Transpaso emitido');
+INSERT INTO TipoMovimiento VALUES(12,'Transpaso recibido');
+INSERT INTO TipoMovimiento VALUES(13,'Pago recibo');
+INSERT INTO TipoMovimiento VALUES(50,'Abono');
+INSERT INTO TipoMovimiento VALUES(51,'Cobro de cheque');
+INSERT INTO TipoMovimiento VALUES(99,'Otros');
+
+
+-- Estados del banco
+
+INSERT INTO EstadoBanco VALUES(1,'Abierta');
+INSERT INTO EstadoBanco VALUES(2,'Cerrada');
+INSERT INTO EstadoBanco VALUES(3,'Trafico detenido');
+INSERT INTO EstadoBanco VALUES(4,'En recuperacion');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
